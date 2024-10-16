dd72ba4c61003f4dd8f67f9b74bd1c5db8ddc1f9
Nicholas Cardoso <nicholas.cardoso@konia.com.br>
2024-10-16 09:38:10 -0300

    feat: configuration key vaults request Azure

--- src/main/kotlin/br/com/hashmapper/HashBrainAI/services/OpenAIClient.kt
+++ src/main/kotlin/br/com/hashmapper/HashBrainAI/services/OpenAIClient.kt
@@ -1,6 +1,7 @@
 package br.com.hashmapper.HashBrainAI.services
 
 import br.com.hashmapper.HashBrainAI.configuration.OpenAIConfig
+import br.com.hashmapper.HashBrainAI.configuration.OpenAIService
 import br.com.hashmapper.HashBrainAI.dto.Speech
 import com.azure.ai.openai.OpenAIClientBuilder
 import com.azure.ai.openai.models.ChatCompletionsOptions
@@ -11,12 +12,13 @@ import org.springframework.stereotype.Service
 
 @Service
 class OpenAIClient(
-//    private val openAIService: OpenAIService
     private val openAiConfig: OpenAIConfig
 ) {
+    private val keys = OpenAIService(openAiConfig)
+
     private val client = OpenAIClientBuilder()
-        .credential(AzureKeyCredential(openAiConfig.apiKey))
-        .endpoint(openAiConfig.endpoint)
+        .credential(AzureKeyCredential(keys.getSecret("ApiKey")))
+        .endpoint(keys.getSecret("Endpoint"))
         .buildClient()
 
     fun checkProfanity(input: Speech): Boolean? {
@@ -24,7 +26,7 @@ class OpenAIClient(
         chatMessages.add(ChatRequestSystemMessage("Por favor, analise o seguinte texto e diga se ele contém palavras de baixo calão ou xingamentos: \"$input\". Responda apenas com 'Sim' se houver e 'Não' se não houver."))
 
         val chatCompletionsOptions = ChatCompletionsOptions(chatMessages)
-        val chatCompletions = client.getChatCompletions(openAiConfig.model, chatCompletionsOptions)
+        val chatCompletions = client.getChatCompletions(keys.getSecret("Model"), chatCompletionsOptions)
 
         val responseContext = chatCompletions.choices[0].message.content.trim()
         println(responseContext)
